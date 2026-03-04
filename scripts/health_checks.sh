35f97c01aabef894fb9c07d53463755d7c3428f5
Bhaskar <boss@localhost.localdomain>
2026-03-04 09:39:17 -0800

    added threshold check and exit codes

--- scripts/health_checks.sh
+++ scripts/health_checks.sh
@@ -1,26 +1,18 @@
 #!/bin/bash
+# variables
+THRESHOLD=80
 LOG_FILE="/var/log/health_checks.log"
 # Function to log messages.
 log_message() {
     echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" | tee -a "$LOG_FILE"
 }
-log_message "=============Starting system health checks================="
-# Disk usage check
-log_message "Starting Disk Usage check..."
-df -h | tee -a "$LOG_FILE"
-# Top commands check
-log_message "Starting TOP commands check..."
-top -b -n 1 | head -n 20 | tee -a "$LOG_FILE"
-# Memory usage check
-log_message "Starting Memory Usage check..."
-free -h | tee -a "$LOG_FILE"
-# CPU usage check
-log_message "Starting CPU Usage check..."
-mpstat | tee -a "$LOG_FILE"
-# Network connectivity check
-log_message "Starting Network Connectivity check..."
-ping -c 4 google.com | tee -a "$LOG_FILE"
-# All systemd service check
-log_message "Starting systemd service check..."
-systemctl list-units --type=service --state=running | tee -a "$LOG_FILE"
-log_message "=============Completed system health checks================="
+log_message "Starting disk usage check..."
+# Check disk usage and alert if it exceeds the threshold.
+USAGE=$(df -h / | awk 'NR==2 {print $5}' | sed 's/%//')
+if [ "$USAGE" -gt "$THRESHOLD" ]; then
+    log_message "Disk usage is at ${USAGE}%, which exceeds the threshold of ${THRESHOLD}%."
+    exit 1
+else
+    log_message "Disk usage is at ${USAGE}%, which is within the acceptable range."
+    exit 0
+fi
